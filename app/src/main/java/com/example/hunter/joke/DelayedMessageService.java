8e2031988477d9c78a2d859fb50c7f09bc08975d
rahul-soshte <rahul.soshte47@gmail.com>
2017-04-09 13:31:41 +0530

    Moving On Commit

--- app/src/main/java/com/example/hunter/joke/DelayedMessageService.java
+++ app/src/main/java/com/example/hunter/joke/DelayedMessageService.java
@@ -1,38 +1,58 @@
 package com.example.hunter.joke;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class DelayedMessageService extends IntentService {
-public static final String EXTRA_MESSAGE="message";
+    public static final String EXTRA_MESSAGE = "message";
+    public static final int NOTIFICATION_ID = 5453;
 
     public DelayedMessageService() {
-super("DelayedMessageService");
+        super("DelayedMessageService");
 
     }
 
-   @Override
-    protected void onHandleIntent(Intent intent){
-       //Code to do something
-   synchronized (this)
-   {
-       try{
-           wait(10000);
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //Code to do something
+        synchronized (this) {
+            try {
+                wait(10000);
 
-       }
-       catch (InterruptedException e){
-           e.printStackTrace();
-       }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-   }
-       String text=intent.getStringExtra(EXTRA_MESSAGE);
-       showText(text);
+        }
+        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        showText(text);
 
-   }
-    private void showText(final String text)
-    {
-        Log.v("DelayedMessageService","The message is "+text);
+    }
+
+
+    private void showText(final String text) {
+Intent intent=new Intent(this,MainActivity.class);
+        TaskStackBuilder stackBuilder= TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent=stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification=new Notification.Builder(this)
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setContentTitle(getString(R.string.app_name))
+        .setAutoCancel(true)
+        .setPriority(Notification.PRIORITY_MAX)
+        .setDefaults(Notification.DEFAULT_VIBRATE)
+        .setContentIntent(pendingIntent)
+        .setContentText(text)
+        .build();
+
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID,notification);
 
     }
 
